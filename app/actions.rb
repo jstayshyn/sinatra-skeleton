32d7571545e0297b22a417cf9d5639c6fec7c135
jstayshyn <justinstayshyn@gmail.com>
2015-06-03 19:50:29 -0400

    class lesson

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+get '/users/new' do
+  erb :"users/new"
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @user = User.find_by({email: params[email]})
+  if @user 
+    if @user.password == params[:password]
+      #log user in
+      session[@user_id] = @user_id
+      redirect '/profile'
+    end  
+  else
+    # user was not found
+    # or password was incorrect
+    redirect 'login'
+  end
+end
+
+
+get 'movies' do
+  erb :movie
+end
+
+
